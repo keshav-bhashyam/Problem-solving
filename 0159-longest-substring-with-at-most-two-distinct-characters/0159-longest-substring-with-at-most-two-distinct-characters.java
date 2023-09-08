e8bc98e6dc6f2a4b0bd8306c210743b2d6c4302b
Keshav Bhashyam <keshav-bhashyam@users.noreply.github.com>
2023-09-08 16:33:40 +0530

    Time: 65 ms (34.13%), Space: 44.7 MB (9.33%) - LeetHub

---
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        int l = 0, r = 0, n = s.length(), longestRun = 0;
+        Map<Character, Integer> counter = new HashMap<>();
+
+        while ( r < n ) {
+            counter.put(s.charAt(r), counter.getOrDefault(s.charAt(r), 0) + 1);
+
+            if (counter.size() > 2) {
+                counter.put(s.charAt(l), counter.getOrDefault(s.charAt(l), 0) - 1);
+                if (counter.get(s.charAt(l)) == 0) {
+                    counter.remove(s.charAt(l));
+                }
+                l++;
+            }
+
+            longestRun = Math.max(longestRun, r - l + 1);
+            r++;
+        }
+
+        return longestRun;   
+    }
+}
