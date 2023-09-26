a3eae8ddf9a073acabaf005ceb9df9ffa0fa5c37
Keshav Bhashyam <keshav-bhashyam@users.noreply.github.com>
2023-09-26 17:19:23 +0530

    Time: 12 ms (28.8%), Space: 46.3 MB (95.22%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
+         int n = nums.length;
         if (n == 1) return nums[0];
 
         Map<Integer, Integer> frequency = new HashMap<>();
@@ -8,13 +8,11 @@ class Solution {
         for (int num : nums) {
             frequency.put(num, frequency.getOrDefault(num, 0) + 1);
         }
-        Map<Integer, Integer> collect = frequency.entrySet()
-                .stream()
-                .max((o1, o2) -> o1.getValue().compareTo(o2.getValue()))
-                .stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey, Map.Entry::getValue));
 
-        return collect.keySet().stream().findFirst().get();
+        return frequency.entrySet()
+                .stream()
+                .max((a, b) -> a.getValue().compareTo(b.getValue()))
+                .get()
+                .getKey();
     }
 }
